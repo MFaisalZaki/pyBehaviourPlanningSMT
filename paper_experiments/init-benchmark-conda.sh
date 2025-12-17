265ac5709b81419d760264a78e0870936cf9e68d
Mustafa Faisal <mustafafaisal@Mustafas-MacBook-Pro-3.local>
2025-12-17 10:32:28 +0000

    more fixes for conda.

--- paper_experiments/init-benchmark-conda.sh
+++ paper_experiments/init-benchmark-conda.sh
@@ -5,28 +5,28 @@ else
     exit 1
 fi
 
-python3.12 -m venv $(pwd)/venv
-source $(pwd)/venv/bin/activate
-pip install setuptools
-pip install unified-planning
-pip install --no-cache up-symk
-pip install git+https://github.com/MFaisalZaki/forbiditerative.git
-pip install lark
-pip install up-fast-downward
-pip install matplotlib 
-pip install seaborn
-pip install .
+condaenvname=behaviour_planning_smt
 
-# pip uninstall pypmt -y
-# pip install git+https://github.com/pyPMT/pyPMT.git@d44efb71746b3a91e7fb1926b4405bd14f9df33b
+conda create -n $condaenvname python=3.12 -y
+conda activate $condaenvname
+
+python -m pip install setuptools
+python -m pip install unified-planning
+python -m pip install --no-cache up-symk
+python -m pip install git+https://github.com/MFaisalZaki/forbiditerative.git
+python -m pip install lark
+python -m pip install up-fast-downward
+python -m pip install matplotlib 
+python -m pip install seaborn
+python -m pip install .
 
 mkdir -p $(pwd)/sandbox-benchmark/classical-domains
 git clone --recursive https://github.com/AI-Planning/classical-domains.git $(pwd)/sandbox-benchmark/classical-domains
 git clone --recursive https://github.com/pyPMT/numeric-domains.git $(pwd)/sandbox-benchmark/numeric-domains
 
-python $(pwd)/paper_experiments/generate-benchmark-slurm-tasks.py --use-conda --conda-name behaviour_planning_smt --planning-type classical        --sandbox-dir $(pwd)/sandbox-benchmark-classical        --planning-tasks-dir $(pwd)/sandbox-benchmark/classical-domains/classical --resources-dir $(pwd)/paper_experiments/data/classical-domains-ru-info
-python $(pwd)/paper_experiments/generate-benchmark-slurm-tasks.py --use-conda --conda-name behaviour_planning_smt --planning-type oversubscription --sandbox-dir $(pwd)/sandbox-benchmark-oversubscription --planning-tasks-dir $(pwd)/sandbox-benchmark/classical-domains/classical
-python $(pwd)/paper_experiments/generate-benchmark-slurm-tasks.py --use-conda --conda-name behaviour_planning_smt --planning-type numerical        --sandbox-dir $(pwd)/sandbox-benchmark-numerical        --planning-tasks-dir $(pwd)/sandbox-benchmark/numeric-domains             --resources-dir $(pwd)/paper_experiments/data/functions-domains-info
+python $(pwd)/paper_experiments/generate-benchmark-slurm-tasks.py --use-conda --conda-name $condaenvname --planning-type classical        --sandbox-dir $(pwd)/sandbox-benchmark-classical        --planning-tasks-dir $(pwd)/sandbox-benchmark/classical-domains/classical --resources-dir $(pwd)/paper_experiments/data/classical-domains-ru-info
+python $(pwd)/paper_experiments/generate-benchmark-slurm-tasks.py --use-conda --conda-name $condaenvname --planning-type oversubscription --sandbox-dir $(pwd)/sandbox-benchmark-oversubscription --planning-tasks-dir $(pwd)/sandbox-benchmark/classical-domains/classical
+python $(pwd)/paper_experiments/generate-benchmark-slurm-tasks.py --use-conda --conda-name $condaenvname --planning-type numerical        --sandbox-dir $(pwd)/sandbox-benchmark-numerical        --planning-tasks-dir $(pwd)/sandbox-benchmark/numeric-domains             --resources-dir $(pwd)/paper_experiments/data/functions-domains-info
 
 sh $(pwd)/paper_experiments/split-slurm.sh $(pwd)/sandbox-benchmark-classical/slurm-dumps $(pwd)/sandbox-benchmark-classical/splitted-slurm-dumps classical $slurmpartname
 sh $(pwd)/paper_experiments/split-slurm.sh $(pwd)/sandbox-benchmark-oversubscription/slurm-dumps $(pwd)/sandbox-benchmark-oversubscription/splitted-slurm-dumps osp $slurmpartname
